6f74b9249a1fc3c7362679ee2ea7d95e9c2f7d27
Amir722806 <amirsohail03030@gmail.com>
2026-05-21 23:37:01 +0500

    QR Code Genrate

--- Index.py
+++ Index.py
@@ -4,3 +4,16 @@
 import qrcode
 import image
 
+qr= qrcode.QRCode(
+    version=15,
+    box_size=10,
+    border=5
+)
+
+data= "https://www.linkedin.com/in/h-amir-sohail-yazdan-maneka-6b4925256/"
+# in link i add my Linkdin Profile Data
+
+qr.add_data(data)
+qr.make(fit=True)
+img=qr.make_image(fill="black", back_color="white")
+img.save("test.png")
